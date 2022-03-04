9b1e2c20f5a2676affaa1a613de69191cd2b85b5
AlenaBlinova <oleg.olegovich.rrrr@gamil.com>
2022-03-04 10:52:23 +0300

    Set up tests

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
     client: 'pg',
     connection: { 
-        host : '127.0.0.1',
-        port : 5432,
-        user : 'postgres',
-        password : '09022004',
-        database : 'handicraft'
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT,
+        user : process.env.DB_USERNAME,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_DATABASE
     },
     migrations: {
         tableName: 'migrations'
+    },
+    seeds: {
+        directory: "./seeds"
     }
 };

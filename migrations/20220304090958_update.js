c7788d8809cdd02d939a5d4b2e460fd0a0d32071
AlenaBlinova <oleg.olegovich.rrrr@gamil.com>
2022-03-11 11:02:33 +0300

    Adding migrations to the categories table

---
+++ migrations/20220304090958_update.js
@@ -0,0 +1,29 @@
+exports.up = function (knex) {
+    await knex.schema.createTable("categories", (table) => {
+        table.increments("id");
+        table.string("name").notNullable();
+        table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+        table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+    });
+
+    await knex.schema.createTable("products_categories", (table) => {
+        table.increments("id");
+        table.integer("category_id").notNullable();
+        table.integer("product_id").notNullable();
+        table
+            .foreign("product_id")
+            .references("products.id")
+            .onDelete("RESTRICT")
+            .onUpdate("CASCADE");
+        table
+            .foreign("category_id")
+            .references("categories.id")
+            .onDelete("RESTRICT")
+            .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function (knex) {
+    await knex.schema.dropTableIfExists("products_categories");
+    await knex.schema.dropTableIfExists("categories");
+};

46f9c96aeb7ca51fc608a5a630ef4583b1d001de
AlenaBlinova <oleg.olegovich.rrrr@gamil.com>
2022-03-24 12:22:51 +0300

    Set up tests

--- seeds/init.js
+++ seeds/init.js
@@ -1,6 +1,8 @@
 exports.seed = async function (knex) {
+    await knex("products_categories").del();
     await knex("reviews_products").del();
     await knex("orders_products").del();
+    await knex("categories").del();
     await knex("photos").del();
     await knex("reviews").del();
     await knex("products").del();
@@ -16,6 +18,36 @@ exports.seed = async function (knex) {
             email_is_confirmed: true,
             password: "1123456",
             role: "admin"
+        },
+    ]);
+    await knex("users").insert([
+        {
+            id: 2,
+            name: "Polina",
+            email: "polina1000@yandex.ru",
+            email_is_confirmed: true,
+            password: "polinA1",
+            role: "user"
+        },
+    ]);
+    await knex("users").insert([
+        {
+            id: 3,
+            name: "Antony",
+            email: "1234@yandex.ru",
+            email_is_confirmed: true,
+            password: "anTOY",
+            role: "user"
+        },
+    ]);
+    await knex("users").insert([
+        {
+            id: 4,
+            name: "Joel",
+            email: "qwJoel3@yandex.ru",
+            email_is_confirmed: true,
+            password: "123321",
+            role: "user"
         },
     ]);
     await knex("orders").insert([
@@ -43,7 +75,7 @@ exports.seed = async function (knex) {
             id: 1,
             name: "Кот из бисера",
             photogallery_id: 1,
-            price: "500",
+            price: 500,
             structure: "Состав: чешский бисер, синтепон, леска. Размеры: высота - 7см, ширина - 5см, длина - 10см.",
             description: "Описание: чёрно-белый кот, зелёные глаза, длинный хвост, в лежачем положении.",
             rating: "5"
@@ -70,6 +102,37 @@ exports.seed = async function (knex) {
         {
             id: 1,
             review_id: 1,
+            product_id: 1
+        },
+    ]);
+    await knex("categories").insert([
+        {
+            id: 1,
+            name: "Вязание"
+        },
+    ]);
+    await knex("categories").insert([
+        {
+            id: 2,
+            name: "Бисероплетение"
+        },
+    ]);
+    await knex("categories").insert([
+        {
+            id: 3,
+            name: "Валяние"
+        },
+    ]);
+    await knex("categories").insert([
+        {
+            id: 4,
+            name: "Шитьё"
+        },
+    ]);
+    await knex("products_categories").insert([
+        {
+            id: 1,
+            category_id: 1,
             product_id: 1
         },
     ]);
